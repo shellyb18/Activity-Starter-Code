a4152b034ffe2ee34d1cf71d747591a51ca6d3b1
shelbaltz <shelbaltz@192.168.1.16>
2017-01-22 20:37:54 -0700

    10

---
+++ ActivityStarterCode/src/Activity10/ElevensGUIRunner.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Activity10;
+
+/**
+ * This is a class that plays the GUI version of the Elevens game.
+ * See accompanying documents for a description of how Elevens is played.
+ */
+public class ElevensGUIRunner {
+
+	/**
+	 * Plays the GUI version of Elevens.
+	 * @param args is not used.
+	 */
+	public static void main(String[] args) {
+		Board board = new ElevensBoard();
+		CardGameGUI gui = new CardGameGUI(board);
+		gui.displayGame();
+	}
+}
